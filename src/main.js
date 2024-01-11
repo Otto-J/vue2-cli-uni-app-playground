dd29a7c1d0b233a93a367d945eaa18d53a877c2f
jixinbao <jixinbao@dcloud.io>
2024-01-11 15:32:46 +0800

    国际化基础功能

--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
+import VueI18n from "vue-i18n";
+const messages = {
+  en: {
+    "schema.name": "Name",
+    message: {
+      hello: "hello world",
+    },
+  },
+  ja: {
+    "schema.name": "メッセージ",
+    message: {
+      hello: "こんにちは、世界",
+    },
+  },
+  "zh-Hans": {
+    "schema.name": "姓名",
+    message: {
+      hello: "你好，世界",
+    },
+  },
+};
+let i18nConfig = {
+  // locale: "en", // 获取已设置的语言
+  locale: uni.getLocale(), // 获取已设置的语言
+  fallbackLocale: "en", // set fallback locale
 
-Vue.config.productionTip = false
+  messages,
+};
+Vue.use(VueI18n);
+const i18n = new VueI18n(i18nConfig);
+App.mpType = "app";
+Vue.config.productionTip = false;
 
-App.mpType = 'app'
+App.mpType = "app";
 
 const app = new Vue({
-  ...App
-})
-app.$mount()
+  i18n,
+  ...App,
+});
+app.$mount();
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,22 +1,46 @@
 <template>
   <view class="content">
-    <div>hi</div>
+    <!-- <div>hi</div> -->
+    <!-- <p>{{ $t("message.hello", { msg: "hello" }) }}</p> -->
+    <!-- <p v-html="$t('message.hello2')"></p> -->
+    <view class="title">{{ $t("schema.name") }}</view>
     <uni-card :title="title" extra="额外信息">
       <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
     </uni-card>
+    <button @tap="changeLanguage">change language</button>
     <!-- <uni-badge text="uni - badge" type="default" size="normal" /> -->
   </view>
 </template>
 
 <script>
+// const messages = {
+//   en: {
+//     message: {
+//       hello: "{msg} world",
+//       hello2: "hello <br> world",
+//     },
+//   },
+// };
+
 export default {
   data() {
     return {
       title: "Hello",
+      // locals: "en",
+      // messages,
     };
   },
   onLoad() {},
-  methods: {},
+  methods: {
+    changeLanguage() {
+      // en - zh-Hans - ja 循环切换
+      const locales = ["en", "zh-Hans", "ja"];
+      const index = locales.findIndex((item) => item === this.$i18n.locale);
+      const nextIndex = (index + 1) % locales.length;
+      // locale.value = locales[nextIndex];
+      this.$i18n.locale = locales[nextIndex];
+    },
+  },
 };
 </script>
 
